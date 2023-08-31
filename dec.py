ac84a1e672907cbe2c6eff360a1859f6e73de320
godmnathan <godmnathan.code@gmail.com>
2023-08-31 10:59:24 -0300

    Decrypt function created

--- dec.py
+++ dec.py
@@ -4,6 +4,7 @@ import sys
 
 dirDefault = 'Archives'
 ignoreFileList = [os.path.basename(__file__), 'key.key', 'enc.py', 'dec.py']
+keyPath = 'key.key'
 
 def listFiles(baseDir):
     allFiles = []
@@ -15,13 +16,38 @@ def listFiles(baseDir):
             allFiles.append(fullPath)
     return allFiles
 
-def main():
+def readKey(keyPath):
+    if not os.path.isfile(keyPath):
+        print(f'Cryptography key not found: {keyPath}')
+        sys.exit(1)
+    with open(keyPath,'rb') as keyFile:
+        key = keyFile.read()
+    return key
 
+def decryptFiles(key,allFiles):
+    filesToDecrypt = []
+
+    for file in allFiles:
+        try:
+            with open(file,'rb') as encryptedFile:
+                fileContent = encryptedFile.read()
+            decryptedFileContent = Fernet(key).decrypt(fileContent)
+            with open(file,'wb') as decryptedFile:
+                decryptedFile.write(decryptedFileContent)
+            filesToDecrypt.append(file)
+        except Exception as e:
+            print(f'Error when decrypting file: {e}')
+    return filesToDecrypt
+
+def main():
     dir = sys.argv[1] if len(sys.argv) > 1 else dirDefault
     archives = listFiles(dir)
 
-    for archive in archives:
-        print(f'{archive}')
+    key = readKey(keyPath)
+
+    decryptedFiles = decryptFiles(key,archives)
+
+    print(f'{decryptedFiles}')
 
 if __name__ == '__main__':
     main()
